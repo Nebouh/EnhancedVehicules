19dbf1205e2d036543a0368c9c55ef094891b11b
Nebouh <aureliendacruz45@hotmail.com>
2025-04-25 01:45:57 +0200

    feat: update system

---
+++ Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EnhancedVehiculesMod
+{
+    [Serializable]
+    public class Config
+    {
+        public Car[] Cars;
+
+        [Serializable]
+        public class Car
+        {
+            public string name;
+            public int price; // Nouveau champ ajouté pour EnhancedVehicules
+        }
+    }
+}
